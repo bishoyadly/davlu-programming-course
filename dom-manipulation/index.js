274e20d679fe62c0a93b861232a9f58062f4ad3a
Bishoy Adly <bishoy.adly.danial@gmail.com>
2023-09-04 01:27:53 +0300

    Adding body animations

--- dom-manipulation/index.js
+++ dom-manipulation/index.js
@@ -1,11 +1,14 @@
+let header1 = document.querySelector('.pageTitle');
 let button1 = document.querySelector('.button1');
 let image1 = document.querySelector('.image1');
-
+let body = document.querySelector('body');
 
 button1.addEventListener('click', function () {
 
+    header1.classList.toggle('header1');
     image1.classList.toggle('hidden');
     button1.classList.toggle('buttonClick');
+    body.classList.toggle('page');
 
     let list = image1.classList;
 
