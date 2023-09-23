d9262175107e4a364e006aa71396ec4e8fc5d50e
Bishoy Adly <bishoy.adly@linkdev.com>
2023-09-23 15:27:06 +0300

    implement game logic

--- script.js
+++ script.js
@@ -1,30 +1,61 @@
-let secretNumber = Math.floor(Math.random() * 20) + 1;
+let secretNumber = Math.floor(Math.random() * 20 + 1);
+let score = 5;
+let highscore = 0;
 
-console.log('computer selected number is : ', secretNumber);
-
-let checkButton = document.querySelector('.btn.check');
+document.querySelector('.score').textContent = score;
 let message = document.querySelector('.message');
-let againButton = document.querySelector('.again');
 
-checkButton.addEventListener('click', function () {
-    let guessedNumber = Number(document.querySelector('.guess').value);
-    console.log('guess number is : ', guessedNumber);
+console.log('secret number is : ', secretNumber);
 
-    if (guessedNumber == 0) {
+let checkButton = document.querySelector(".btn.check");
+
+checkButton.addEventListener("click", function () {
+
+    let guessedNumber = parseInt(document.querySelector(".guess").value);
+    console.log('guessed number is :', guessedNumber);
+
+    if (isNaN(guessedNumber)) {
         message.textContent = '⛔️ No number!';
     }
-    else if (guessedNumber > secretNumber) {
-        message.textContent = '📈 Too high!';
-    } else if (guessedNumber < secretNumber) {
-        message.textContent = '📉 Too low!';
-    } else {
+
+    if (guessedNumber == secretNumber) {
+        highscore += score;
         message.textContent = '🎉 Correct Number!';
         document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.highscore').textContent = highscore;
+    } else {
+        if (score > 1) {
+
+            if (guessedNumber > secretNumber) {
+                score--;
+                document.querySelector('.score').textContent = score;
+                message.textContent = '📈 Too high!';
+            }
+
+            else if (guessedNumber < secretNumber) {
+                score--;
+                document.querySelector('.score').textContent = score;
+                message.textContent = '📉 Too low!';
+            }
+
+        } else {
+            message.textContent = '💥 You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
     }
+
 });
 
+
+let againButton = document.querySelector('.again');
+
 againButton.addEventListener('click', function () {
-    secretNumber = Math.floor(Math.random() * 20) + 1;
-    console.log('computer selected number is : ', secretNumber);
+
+    secretNumber = Math.floor(Math.random() * 20 + 1);
+    console.log('new secret number is : ', secretNumber);
+    document.querySelector('body').style.backgroundColor = '#222';
     message.textContent = 'Start guessing...';
+    score = 5;
+    document.querySelector('.score').textContent = 5;
+
 });
