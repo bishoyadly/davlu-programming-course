83dcd0b1f4f4cf7c9cb9ff8ad4b72b19ce1f7b0c
Bishoy Adly <bishoy.adly@linkdev.com>
2024-11-28 15:52:50 +0200

    add prompt commands to add numbers

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Javascript Programming Course");
+let number1 = prompt('enter number 1 ');
+number1 = parseInt(number1);
+let number2 = prompt('enter number 2 ');
+number2 = parseInt(number2);
+
+console.log(number1+number2);
